1473de5c07851e3fb1b59dea355f137200d38765
Mylena Monte <mylena.monte@liferay.com>
2026-03-18 11:57:46 -0300

    LPD-221 Add Authorization Token on MessageResourceTest.testPostChatByExternalReferenceCodeMessage

--- modules/dxp/apps/ai-hub/ai-hub-rest-test/src/testIntegration/java/com/liferay/ai/hub/rest/resource/v1_0/test/MessageResourceTest.java
+++ modules/dxp/apps/ai-hub/ai-hub-rest-test/src/testIntegration/java/com/liferay/ai/hub/rest/resource/v1_0/test/MessageResourceTest.java
@@ -6,21 +6,31 @@
 package com.liferay.ai.hub.rest.resource.v1_0.test;
 
 import com.liferay.ai.hub.cell.security.JWTTokenUtil;
+import com.liferay.ai.hub.configuration.AIHubConfiguration;
 import com.liferay.ai.hub.rest.resource.v1_0.test.util.SseEventSourceTestUtil;
 import com.liferay.ai.hub.rest.resource.v1_0.util.SseUtil;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.oauth2.provider.constants.ClientProfile;
+import com.liferay.oauth2.provider.constants.GrantType;
+import com.liferay.oauth2.provider.model.OAuth2Application;
+import com.liferay.oauth2.provider.service.OAuth2ApplicationLocalService;
+import com.liferay.oauth2.provider.util.OAuth2SecureRandomGenerator;
+import com.liferay.portal.configuration.test.util.ConfigurationTestUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.util.HTTPTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.Http;
 import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.Inject;
@@ -28,6 +38,7 @@ import com.liferay.site.initializer.SiteInitializer;
 import com.liferay.site.initializer.SiteInitializerRegistry;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
@@ -84,13 +95,14 @@ public class MessageResourceTest extends BaseMessageResourceTestCase {
 		SseUtil.closeAll();
 	}
 
-	@Ignore
 	@Override
 	@Test
 	public void testPostChatByExternalReferenceCodeMessage() throws Exception {
 		CountDownLatch countDownLatch1 = new CountDownLatch(4);
 		CountDownLatch countDownLatch2 = new CountDownLatch(6);
 		List<String> lines = new ArrayList<>();
+
+		JSONObject token = _generateToken();
 
 		String sseEventSinkKey = SseEventSourceTestUtil.open(
 			List.of(countDownLatch1, countDownLatch2), lines,
@@ -105,8 +117,11 @@ public class MessageResourceTest extends BaseMessageResourceTestCase {
 			"ai-hub/v1.0/chats/by-external-reference-code/" + sseEventSinkKey +
 				"/messages",
 			HashMapBuilder.put(
-				"Liferay-AI-Hub-Cell-On-Behalf-Of",
-				_generateToken(TestPropsValues.getUserId())
+				"Liferay-AI-Hub-On-Behalf-Of",
+				token.getString("userToken")
+			).put(
+				"Authorization",
+				"Bearer " + token.getString("accessToken")
 			).build(),
 			Http.Method.POST);
 
@@ -131,8 +146,11 @@ public class MessageResourceTest extends BaseMessageResourceTestCase {
 			"ai-hub/v1.0/chats/by-external-reference-code/" + sseEventSinkKey +
 				"/messages",
 			HashMapBuilder.put(
-				"Liferay-AI-Hub-Cell-On-Behalf-Of",
-				_generateToken(TestPropsValues.getUserId())
+				"Liferay-AI-Hub-On-Behalf-Of",
+				token.getString("userToken")
+			).put(
+				"Authorization",
+				"Bearer " + token.getString("accessToken")
 			).build(),
 			Http.Method.POST);
 
@@ -152,10 +170,35 @@ public class MessageResourceTest extends BaseMessageResourceTestCase {
 		Assert.assertTrue(firstMessageSent, firstMessageSent.contains(text));
 	}
 
-	private String _generateToken(long userId) throws Exception {
-		return JWTTokenUtil.generateToken(
-			TimeUnit.MINUTES.toMillis(1), RandomTestUtil.randomString(),
-			userId);
+
+	private JSONObject _generateToken() throws Exception {
+		User user = TestPropsValues.getUser();
+
+		OAuth2Application oAuth2Application =
+			_oAuth2ApplicationLocalService.addOAuth2Application(
+				user.getCompanyId(), user.getUserId(), user.getFullName(),
+				List.of(GrantType.CLIENT_CREDENTIALS), "client_secret_post",
+				user.getUserId(),
+				OAuth2SecureRandomGenerator.generateClientId(),
+				ClientProfile.WEB_APPLICATION.id(),
+				OAuth2SecureRandomGenerator.generateClientSecret(), "",
+				List.of(), "http://localhost:8080", 0, null, "AI Hub", "",
+				List.of("http://localhost:8080"), false,
+				Arrays.asList("Liferay.AI.Hub.REST.everything"), false,
+				new ServiceContext());
+
+		ConfigurationTestUtil.saveConfiguration(
+			AIHubConfiguration.class.getName(),
+			HashMapDictionaryBuilder.<String, Object>put(
+				"clientId", oAuth2Application.getClientId()
+			).put(
+				"clientSecret", oAuth2Application.getClientSecret()
+			).put(
+				"serviceURL", "http://localhost:8080"
+			).build());
+
+		return HTTPTestUtil.invokeToJSONObject(
+			null, "ai-hub/v1.0/tokens", Http.Method.POST);
 	}
 
 	private static String _originalName;
@@ -163,5 +206,8 @@ public class MessageResourceTest extends BaseMessageResourceTestCase {
 
 	@Inject
 	private static SiteInitializerRegistry _siteInitializerRegistry;
+
+	@Inject
+	private OAuth2ApplicationLocalService _oAuth2ApplicationLocalService;
 
 }
