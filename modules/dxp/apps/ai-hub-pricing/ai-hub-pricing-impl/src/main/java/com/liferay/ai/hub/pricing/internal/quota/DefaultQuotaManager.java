c016df73de119278d6dd892bf559f36f0bec1c0c
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-06-09 15:05:01 -0300

    LPD-93148 feat: limit to 10 concurrent agent executions per account
    Co-authored-by: Tina Tian <tina.tian@liferay.com>
    Co-authored-by: Mario Gomes <mario.gomes@liferay.com>

--- modules/dxp/apps/ai-hub-pricing/ai-hub-pricing-impl/src/main/java/com/liferay/ai/hub/pricing/internal/quota/DefaultQuotaManager.java
+++ modules/dxp/apps/ai-hub-pricing/ai-hub-pricing-impl/src/main/java/com/liferay/ai/hub/pricing/internal/quota/DefaultQuotaManager.java
@@ -9,11 +9,13 @@ import com.google.cloud.vertexai.VertexAI;
 import com.google.cloud.vertexai.api.CountTokensResponse;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 
+import com.liferay.account.model.AccountEntry;
 import com.liferay.ai.hub.configuration.VertexAIConfiguration;
 import com.liferay.ai.hub.pricing.internal.converter.util.TokenConverterUtil;
 import com.liferay.ai.hub.pricing.rest.util.ObjectEntryUtil;
 import com.liferay.ai.hub.quota.QuotaManager;
 import com.liferay.ai.hub.quota.Usage;
+import com.liferay.ai.hub.util.AccountEntryUtil;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.rest.dto.v1_0.ObjectEntry;
 import com.liferay.object.rest.manager.v1_0.DefaultObjectEntryManager;
@@ -21,10 +23,16 @@ import com.liferay.object.rest.manager.v1_0.DefaultObjectEntryManagerProvider;
 import com.liferay.object.rest.manager.v1_0.ObjectEntryManagerRegistry;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.petra.concurrent.DCLSingleton;
 import com.liferay.portal.configuration.module.configuration.ConfigurationProviderUtil;
+import com.liferay.portal.kernel.dao.orm.ProjectionFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.lock.DuplicateLockException;
 import com.liferay.portal.kernel.lock.Lock;
 import com.liferay.portal.kernel.lock.LockManagerUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
@@ -35,8 +43,11 @@ import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Time;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+import com.liferay.portal.lock.service.LockLocalService;
 import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+
+import jakarta.persistence.PersistenceException;
 
 import java.io.Closeable;
 import java.io.IOException;
@@ -44,8 +55,14 @@ import java.io.Serializable;
 
 import java.math.BigDecimal;
 
+import java.util.Date;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeoutException;
 
 import org.osgi.service.component.annotations.Component;
@@ -55,7 +72,7 @@ import org.osgi.service.component.annotations.Reference;
  * @author Carolina Barbosa
  */
 @Component(service = QuotaManager.class)
-public class AIHubPricingQuotaManagerImpl implements QuotaManager {
+public class DefaultQuotaManager implements QuotaManager {
 
 	@Override
 	public void addQuotas(long accountEntryId, long companyId, long userId)
@@ -92,7 +109,83 @@ public class AIHubPricingQuotaManagerImpl implements QuotaManager {
 	}
 
 	@Override
-	public void checkUsage(long companyId, long userId) throws PortalException {
+	public Closeable checkConcurrentRequests(long userId)
+		throws PortalException {
+
+		long currentTimeMillis = System.currentTimeMillis();
+
+		Semaphore semaphore = _semaphoreDCLSingleton.getSingleton(
+			() -> new Semaphore(10 - _expirationTimes.size()));
+
+		for (Map.Entry<String, Long> entry : _expirationTimes.entrySet()) {
+			if ((entry.getValue() < currentTimeMillis) &&
+				_expirationTimes.remove(entry.getKey(), entry.getValue())) {
+
+				semaphore.release();
+			}
+		}
+
+		if (!semaphore.tryAcquire()) {
+			throw new UnsupportedOperationException(
+				"You have exceeded your concurrent request limit");
+		}
+
+		AccountEntry accountEntry = AccountEntryUtil.getUserAccountEntry(
+			userId);
+
+		boolean acquired = false;
+
+		try {
+			String prefix = accountEntry.getAccountEntryId() + ":";
+
+			Set<String> keys = _getKeys(prefix);
+
+			String owner = PortalUUIDUtil.generate();
+
+			for (int i = 0; i < 10; i++) {
+				String key = prefix + Math.floorMod(owner.hashCode() + i, 10);
+
+				if (keys.contains(key)) {
+					continue;
+				}
+
+				try {
+					Lock lock = LockManagerUtil.lock(
+						userId, QuotaManager.class.getName(), key, owner, false,
+						Time.MINUTE, false);
+
+					if (Objects.equals(lock.getOwner(), owner)) {
+						_expirationTimes.put(
+							owner, System.currentTimeMillis() + Time.MINUTE);
+
+						acquired = true;
+
+						return () -> _release(lock);
+					}
+				}
+				catch (DuplicateLockException | PersistenceException
+							exception) {
+
+					if (_log.isDebugEnabled()) {
+						_log.debug(exception);
+					}
+				}
+			}
+
+			throw new UnsupportedOperationException(
+				"You have exceeded your concurrent request limit");
+		}
+		finally {
+			if (!acquired) {
+				semaphore.release();
+			}
+		}
+	}
+
+	@Override
+	public void checkTokensUsage(long companyId, long userId)
+		throws PortalException {
+
 		if (ListUtil.isEmpty(
 				ObjectEntryUtil.getQuotaBlockObjectEntries(
 					companyId, _dtoConverterRegistry,
@@ -195,6 +288,33 @@ public class AIHubPricingQuotaManagerImpl implements QuotaManager {
 				objectDefinition.getStorageType()));
 	}
 
+	private Set<String> _getKeys(String keyPrefix) {
+		return new HashSet<>(
+			_lockLocalService.dynamicQuery(
+				_lockLocalService.dynamicQuery(
+				).add(
+					PropertyFactoryUtil.forName(
+						"className"
+					).eq(
+						QuotaManager.class.getName()
+					)
+				).add(
+					PropertyFactoryUtil.forName(
+						"expirationDate"
+					).gt(
+						new Date()
+					)
+				).add(
+					PropertyFactoryUtil.forName(
+						"key"
+					).like(
+						keyPrefix + "%"
+					)
+				).setProjection(
+					ProjectionFactoryUtil.property("key")
+				)));
+	}
+
 	private BigDecimal _getTokenCount(long companyId, Usage usage)
 		throws PortalException {
 
@@ -237,7 +357,7 @@ public class AIHubPricingQuotaManagerImpl implements QuotaManager {
 
 		while (true) {
 			Lock lock = LockManagerUtil.lock(
-				AIHubPricingQuotaManagerImpl.class.getName(),
+				DefaultQuotaManager.class.getName(),
 				String.valueOf(objectEntryId), null, updatedOwner);
 
 			if (Objects.equals(lock.getOwner(), updatedOwner)) {
@@ -261,12 +381,34 @@ public class AIHubPricingQuotaManagerImpl implements QuotaManager {
 		}
 
 		return () -> LockManagerUtil.unlock(
-			AIHubPricingQuotaManagerImpl.class.getName(),
-			String.valueOf(objectEntryId), updatedOwner);
+			DefaultQuotaManager.class.getName(), String.valueOf(objectEntryId),
+			updatedOwner);
 	}
+
+	private void _release(Lock lock) {
+		LockManagerUtil.unlock(
+			QuotaManager.class.getName(), lock.getKey(), lock.getOwner());
+
+		Semaphore semaphore = _semaphoreDCLSingleton.getSingleton(() -> null);
+
+		if ((semaphore != null) &&
+			(_expirationTimes.remove(lock.getOwner()) != null)) {
+
+			semaphore.release();
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DefaultQuotaManager.class);
 
 	@Reference
 	private DTOConverterRegistry _dtoConverterRegistry;
+
+	private final ConcurrentMap<String, Long> _expirationTimes =
+		new ConcurrentHashMap<>();
+
+	@Reference
+	private LockLocalService _lockLocalService;
 
 	@Reference
 	private ObjectDefinitionLocalService _objectDefinitionLocalService;
@@ -276,6 +418,9 @@ public class AIHubPricingQuotaManagerImpl implements QuotaManager {
 
 	@Reference
 	private ObjectEntryManagerRegistry _objectEntryManagerRegistry;
+
+	private final DCLSingleton<Semaphore> _semaphoreDCLSingleton =
+		new DCLSingleton<>();
 
 	@Reference
 	private UserLocalService _userLocalService;
