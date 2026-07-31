23c3ecbb917567f8af804b7d6c31335fc695949c
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-07-30 23:51:04 -0300

    LPD-100052 refactor: content gap analyst should also be able to create content if there is a gap in a tuple of funnel stage and persona
    Co-authored-by: Alberto Sousa <alberto.sousa@liferay.com>

--- workspaces/liferay-aihub-workspace/modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/node/delegate/GenerateGapContentServiceNodeDelegate.java
+++ workspaces/liferay-aihub-workspace/modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/node/delegate/GenerateGapContentServiceNodeDelegate.java
@@ -9,17 +9,15 @@ import com.liferay.ai.hub.agent.AgentContext;
 import com.liferay.ai.hub.agent.DefaultAgent;
 import com.liferay.ai.hub.util.AccountEntryUtil;
 import com.liferay.ai.hub.workflow.node.ServiceNodeDelegate;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.encryptor.EncryptorUtil;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.MapUtil;
-import com.liferay.portal.kernel.util.URLCodec;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowNodeManager;
 import com.liferay.portal.workflow.kaleo.model.KaleoInstanceToken;
 import com.liferay.portal.workflow.kaleo.runtime.ExecutionContext;
@@ -28,17 +26,16 @@ import java.io.Serializable;
 
 import java.util.Arrays;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Alberto Sousa
+ * @author Feliphe Marinho
  */
 @Component(service = ServiceNodeDelegate.class)
-public class GenerateContentServiceNodeDelegate implements ServiceNodeDelegate {
+public class GenerateGapContentServiceNodeDelegate
+	implements ServiceNodeDelegate {
 
 	@Override
 	public String execute(
@@ -49,15 +46,16 @@ public class GenerateContentServiceNodeDelegate implements ServiceNodeDelegate {
 
 		KaleoInstanceToken kaleoInstanceToken =
 			executionContext.getKaleoInstanceToken();
+		ServiceContext serviceContext = executionContext.getServiceContext();
 
 		Company company = _companyLocalService.getCompany(
 			kaleoInstanceToken.getCompanyId());
 
-		String generatedContent = String.valueOf(
+		String output = String.valueOf(
 			_defaultAgent.invoke(
 				AgentContext.builder(
 				).agentDefinitionExternalReferenceCode(
-					"L_GENERATE_CONTENT"
+					"L_GENERATE_GAP_CONTENT"
 				).asynchronous(
 					false
 				).companyId(
@@ -67,28 +65,38 @@ public class GenerateContentServiceNodeDelegate implements ServiceNodeDelegate {
 						kaleoInstanceToken.getUserId())
 				).input(
 					HashMapBuilder.<String, Object>put(
-						"brief", inputVariables.get("brief")
+						"count", 1
 					).put(
-						"count", inputVariables.get("count")
-					).put(
-						"funnelStageId", inputVariables.get("funnelStageId")
-					).put(
-						"objectDefinitionName", "CMSBasicWebContent"
+						"gaps",
+						() -> {
+							JSONObject jsonObject =
+								_jsonFactory.createJSONObject(
+									MapUtil.getString(
+										workflowContext, "analysisResult"));
+
+							return jsonObject.getJSONArray(
+								"gaps"
+							).toString();
+						}
 					).put(
 						"objectFields", inputVariables.get("objectFields")
 					).put(
-						"personaId", inputVariables.get("personaId")
-					).put(
 						"projectId", inputVariables.get("projectId")
 					).put(
-						"spaceId", inputVariables.get("spaceId")
+						"projectScopeKey", inputVariables.get("projectScopeKey")
+					).put(
+						"spacesJSONArray", inputVariables.get("spacesJSONArray")
 					).build()
 				).inputVariableNames(
-					Arrays.asList("brief", "count")
+					Arrays.asList(
+						"count", "gaps", "objectFields", "projectId",
+						"projectScopeKey", "spacesJSONArray")
 				).oAuth2ApplicationId(
 					MapUtil.getLong(workflowContext, "oAuth2ApplicationId")
 				).serviceContext(
-					executionContext.getServiceContext()
+					serviceContext
+				).sseEventSinkKey(
+					MapUtil.getString(workflowContext, "sseEventSinkKey")
 				).userId(
 					kaleoInstanceToken.getUserId()
 				).userToken(
@@ -96,74 +104,21 @@ public class GenerateContentServiceNodeDelegate implements ServiceNodeDelegate {
 						company.getKeyObj(),
 						GetterUtil.getString(workflowContext.get("userToken")))
 				).workflowDefinitionName(
-					"Generate Content"
+					"Generate Gap Content"
 				).build()));
 
-		_completeWorkflowNode(
-			executionContext, generatedContent, inputVariables,
-			workflowContext);
+		workflowContext.put("output", output);
 
-		return generatedContent;
+		WorkflowNodeUtil.completeWorkflowNode(
+			executionContext, workflowContext, _workflowNodeManager);
+
+		return output;
 	}
 
 	@Override
 	public String getKey() {
-		return "javaDelegate#generateContent";
+		return "javaDelegate#generateGapContent";
 	}
-
-	private void _completeWorkflowNode(
-			ExecutionContext executionContext, String generatedContent,
-			Map<String, String> inputVariables,
-			Map<String, Serializable> workflowContext)
-		throws Exception {
-
-		workflowContext.put(
-			"classExternalReferenceCode",
-			_getClassExternalReferenceCode(generatedContent));
-		workflowContext.put(
-			"className",
-			_getStringValue(
-				inputVariables.get("objectDefinitionResponse"), "className"));
-		workflowContext.put("generatedContent", generatedContent);
-		workflowContext.put(
-			"groupExternalReferenceCode",
-			GetterUtil.getString(
-				inputVariables.get("spaceExternalReferenceCode")));
-		workflowContext.put("output", generatedContent);
-		workflowContext.put(
-			"scopeKey",
-			GetterUtil.getString(inputVariables.get("projectScopeKey")));
-
-		WorkflowNodeUtil.completeWorkflowNode(
-			executionContext, workflowContext, _workflowNodeManager);
-	}
-
-	private String _getClassExternalReferenceCode(String generatedContent) {
-		if (Validator.isNull(generatedContent)) {
-			return StringPool.BLANK;
-		}
-
-		Matcher matcher = _pattern.matcher(generatedContent);
-
-		if (matcher.find()) {
-			return URLCodec.decodeURL(matcher.group(1));
-		}
-
-		return StringPool.BLANK;
-	}
-
-	private String _getStringValue(String json, String key) throws Exception {
-		if (Validator.isNull(json)) {
-			return StringPool.BLANK;
-		}
-
-		JSONObject jsonObject = _jsonFactory.createJSONObject(json);
-
-		return jsonObject.getString(key);
-	}
-
-	private static final Pattern _pattern = Pattern.compile(
-		"externalReferenceCode=([^&]+)");
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
