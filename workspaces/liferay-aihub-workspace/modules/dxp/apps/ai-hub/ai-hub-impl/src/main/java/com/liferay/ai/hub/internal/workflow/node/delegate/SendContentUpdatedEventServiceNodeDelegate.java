c743d879356873ff0b9c4ec5b599a293c3c9df13
Feliphe Marinho <feliphemarinho18@gmail.com>
2026-08-03 15:37:23 -0300

    LPD-100319 feat: send Content Update SSE event to notify the frontend end inorder to render again the matrix and the grid that lists the contents

---
+++ workspaces/liferay-aihub-workspace/modules/dxp/apps/ai-hub/ai-hub-impl/src/main/java/com/liferay/ai/hub/internal/workflow/node/delegate/SendContentUpdatedEventServiceNodeDelegate.java
@@ -0,0 +1,78 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.ai.hub.internal.workflow.node.delegate;
+
+import com.liferay.ai.hub.rest.resource.v1_0.util.SseUtil;
+import com.liferay.ai.hub.workflow.node.ServiceNodeDelegate;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowNodeManager;
+import com.liferay.portal.workflow.kaleo.model.KaleoInstanceToken;
+import com.liferay.portal.workflow.kaleo.runtime.ExecutionContext;
+
+import java.io.Serializable;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(service = ServiceNodeDelegate.class)
+public class SendContentUpdatedEventServiceNodeDelegate
+	implements ServiceNodeDelegate {
+
+	@Override
+	public String execute(
+			ExecutionContext executionContext,
+			Map<String, String> inputVariables,
+			Map<String, Serializable> workflowContext)
+		throws Exception {
+
+		KaleoInstanceToken kaleoInstanceToken =
+			executionContext.getKaleoInstanceToken();
+
+		SseUtil.send(
+			_getAgentDefinitionExternalReferenceCodes(workflowContext),
+			inputVariables.get("linkResponse"), "Content Updated",
+			kaleoInstanceToken.getCurrentKaleoNodeName(),
+			MapUtil.getString(workflowContext, "sseEventSinkKey"));
+
+		workflowContext.put(
+			"output", "Content successfully created and linked to the project");
+
+		WorkflowNodeUtil.completeWorkflowNode(
+			executionContext, workflowContext, _workflowNodeManager);
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getKey() {
+		return "javaDelegate#sendContentUpdatedEvent";
+	}
+
+	private String[] _getAgentDefinitionExternalReferenceCodes(
+		Map<String, Serializable> workflowContext) {
+
+		String agentDefinitionExternalReferenceCode = GetterUtil.getString(
+			workflowContext.get("agentDefinitionExternalReferenceCode"));
+
+		if (Validator.isNull(agentDefinitionExternalReferenceCode)) {
+			return null;
+		}
+
+		return new String[] {agentDefinitionExternalReferenceCode};
+	}
+
+	@Reference
+	private WorkflowNodeManager _workflowNodeManager;
+
+}
